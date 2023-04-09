3c6aff1fad306880bc769b6e04d929e3c90aa9d2
Never Used Github <81071335+neverUsedGithub@users.noreply.github.com>
2023-04-09 22:25:56 +0200

    reverted

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-
-  return defineConfig({
-    plugins: [ svelte() ],
-    server: {
-      host: true,
-      headers: {
-        "Cross-Origin-Embedder-Policy": "require-corp",
-        "Cross-Origin-Opener-Policy": "same-origin"
-      },
-      port: parseInt(process.env.PORT)
+export default defineConfig({
+  plugins: [ svelte() ],
+  server: {
+    host: true,
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin"
     }
-  })
-}
+  }
+})
